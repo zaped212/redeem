b473f40b34320654dfdaa6558d3c67ffec1921c2
zaped212 <zaped212@gmail.com>
2019-01-13 17:22:09 -0600

    Add custom Deltabot configuration file

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo rm -fr /etc/redeem/*
 sudo cp configs/* /etc/redeem
 sudo cp data/* /etc/redeem
+sudo ln /etc/redeem/Deltabot_Custom.cfg /etc/redeem/printer.cfg -s
 sudo systemctl restart redeem
